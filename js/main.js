af8b481fd88184991ba8596d10bf00253498d79b
jakob <drusany.jakob@gmail.com>
2019-12-23 02:40:10 +0100

    Added line drawing, bounce, not working yet

--- js/main.js
+++ js/main.js
@@ -29,8 +29,13 @@ class Maze {
         );
       }
     }
+    this.calcRawWalls();
   }
   drawMaze() {
+    this.rawWalls.forEach(l => l.drawLine());
+  }
+
+  calcRawWalls() {
     this.rawWalls = [];
     for (let i = 0; i < this.cells.length; i++) {
       for (
@@ -66,19 +71,39 @@ class Maze {
         //console.log(x, y, x1, y1, x2, y2);
 
         if (c.walls[0]) {
-          this.rawWalls.push(new Line({ x: x1, y: y1 }, { x: x2, y: y1 }));
+          this.rawWalls.push(
+            new Line(
+              { x: x1 + Math.random(), y: y1 },
+              { x: x2 + +Math.random(), y: y1 }
+            )
+          );
           //console.log({ x: x1, y: y1 }, { x: x2, y: y1 });
         }
         if (c.walls[1]) {
-          this.rawWalls.push(new Line({ x: x2, y: y1 }, { x: x2, y: y2 }));
+          this.rawWalls.push(
+            new Line(
+              { x: x2 + Math.random(), y: y1 },
+              { x: x2 + +Math.random(), y: y2 }
+            )
+          );
           //console.log({ x: x2, y: y1 }, { x: x2, y: y2 });
         }
         if (c.walls[2]) {
-          this.rawWalls.push(new Line({ x: x1, y: y2 }, { x: x2, y: y2 }));
+          this.rawWalls.push(
+            new Line(
+              { x: x1 + Math.random(), y: y2 },
+              { x: x2 + +Math.random(), y: y2 }
+            )
+          );
           //console.log({ x: x1, y: y2 }, { x: x2, y: y2 });
         }
         if (c.walls[3]) {
-          this.rawWalls.push(new Line({ x: x1, y: y1 }, { x: x1, y: y2 }));
+          this.rawWalls.push(
+            new Line(
+              { x: x1 + Math.random(), y: y1 },
+              { x: x1 + +Math.random(), y: y2 }
+            )
+          );
           //console.log({ x: x1, y: y1 }, { x: x1, y: y2 });
         }
       });
@@ -105,8 +130,8 @@ class Maze {
       }
     });
     this.rawWalls = uniqueWalls;
-    this.rawWalls.forEach(l => l.drawLine());
   }
+
   get cellWidth() {
     return canvas.width / this.cells.length;
   }
@@ -123,17 +148,25 @@ class Cell {
 }
 
 class Line {
-  constructor(a, b, lineMode) {
+  /**
+   *
+   * @param {*} a point a or k
+   * @param {*} b point b or n
+   * @param {*} lineMode if this is true, you can init the Line with k and n
+   * @param {*} pointLineMode if this is true, you can init the line with a and b but they are not saved and a infinite linear function is made
+   */
+  constructor(a, b, lineMode, pointLineMode) {
     if (!lineMode) {
-      this.a = a;
-      this.b = b;
+      if (!pointLineMode) {
+        this.a = a;
+        this.b = b;
+      }
 
-      this.k = (this.b.y - this.a.y) / (this.b.x - this.a.x);
-      this.n =
-        (this.a.x * this.b.y - this.b.x * this.a.y) / (this.a.x - this.b.x);
+      this.k = (b.y - a.y) / (b.x - a.x);
+      this.n = (a.x * b.y - b.x * a.y) / (a.x - b.x);
     } else {
       this.k = a;
-      this.n = n;
+      this.n = b;
     }
   }
 
@@ -144,22 +177,144 @@ class Line {
     ctx.stroke();
   }
 
+  drawLinearFunction() {
+    ctx.beginPath();
+    ctx.moveTo(0, this.eval(0));
+    ctx.lineTo(ctx.canvas.width, this.eval(ctx.canvas.width));
+    ctx.stroke();
+  }
+
+  /**
+   *
+   * @param {Line} wallFunction
+   * @returns a new linear function
+   */
+  getBounceFromWall(wallFunction) {
+    let intersect = this.intersection(wallFunction);
+    let angleBetween = this.angleBetween(wallFunction);
+
+    let newK = Math.tan(Math.atan(wallFunction.k) + angleBetween);
+    let newN = intersect.y - newK * intersect.x;
+    return new Line(newK, newN, true);
+  }
+
+  eval(x) {
+    return this.k * x + this.n;
+  }
+
   intersection(line) {
     if (this.k != line.k) {
       const x = (line.n - this.n) / (this.k - line.k);
-      return { x, y: line.k * x + line.n };
+      const out = { x, y: line.k * x + line.n };
+      if (!(line.intersectionValid(out) && this.intersectionValid(out)))
+        console.log(out, line.intersectionValid(out) ? this : line);
+      if (line.intersectionValid(out) && this.intersectionValid(out))
+        return out;
     }
   }
 
+  intersectionValid(point) {
+    const errorMargin = 0.01;
+    return (
+      (!this.a && !this.b) ||
+      (((point.x >= this.a.x - errorMargin &&
+        point.x <= this.b.x + errorMargin) ||
+        (point.x >= this.b.x - errorMargin &&
+          point.x <= this.a.x + errorMargin)) &&
+        ((point.y >= this.a.y - errorMargin &&
+          point.y <= this.b.y + errorMargin) ||
+          (point.y >= this.b.y - errorMargin &&
+            point.y <= this.a.y + errorMargin)))
+    );
+  }
+
+  distanceToPoint(point) {
+    return (
+      Math.abs(this.n + this.k * point.x - point.y) / Math.sqrt(1 + this.k ** 2)
+    );
+  }
+
   angleBetween(line) {
-    return Math.arctan(Math.abs((this.k - line.k) / (1 + this.k * line.k)));
+    return Math.atan(Math.abs((this.k - line.k) / (1 + this.k * line.k)));
   }
 }
 
+const distanceBetween = (a, b) =>
+  Math.sqrt(Math.pow(a.x - b.x, 2) + Math.pow(a.y - b.y, 2));
+
 const maze1 = new Maze(3);
+const startingPoint = { x: 20, y: 30 };
+const angle = 0.3;
+
+const ray1 = extendLine(startingPoint, angle);
+
+const extendLine = (startPoint, theta) => {
+  const congruentTheta = theta % (Math.PI * 2);
+  const x =
+    congruentTheta > -Math.PI / 2 && congruentTheta < Math.PI / 2
+      ? canvas.width
+      : 0;
+  return new Line(startPoint, {
+    x,
+    y: new Line(
+      startPoint,
+      {
+        x: startPoint.x + Math.cos(congruentTheta) * 2,
+        y: startPoint.y + Math.sin(congruentTheta) * 2
+      },
+      false,
+      true
+    ).eval(x)
+  });
+};
+
+maze1.calcRawWalls();
+
+const bounceLimit = 20;
+const rays = [ray1];
+const intersections = [startingPoint];
+
+let lastAngle = angle;
+let iter = 0;
+while (true) {
+  const lastIntersection = intersections[intersections.length - 1];
+  let closest = maze1.rawWalls
+    .map(w => ({ inter: rays[rays.length - 1].intersection(w), wall: w }))
+    .filter(w => w.inter)
+    .reduce(
+      (prevValue, { inter, wall }) => {
+        const distance = distanceBetween(inter, lastIntersection);
+        if (prevValue.d > distance) {
+          return { d: distance, p: inter, w: wall };
+        } else {
+          return prevValue;
+        }
+      },
+      { d: Infinity }
+    );
+  const newRay = closest.w ? ray1.getBounceFromWall(closest.w) : false;
+  const startAbove = lastIntersection.y > closest.w.eval(lastIntersection.x);
+  const leftAboveWall = newRay.eval(0) < closest.w.eval(0);
+  const rightAboveWall =
+    newRay.eval(canvas.width) < closest.w.eval(canvas.width);
+
+  rays.push(newRay);
+  intersections.push(closest.p);
+  if (
+    !newRay ||
+    bounceLimit < iter ||
+    newRay.distanceToPoint(startingPoint) < 0.01
+  ) {
+    break;
+  }
+  iter++;
+}
 
 const refreshCanvas = () => {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
   maze1.drawMaze();
+
+  rays.forEach(l => l.drawLinearFunction());
 
   requestAnimationFrame(refreshCanvas);
 };
